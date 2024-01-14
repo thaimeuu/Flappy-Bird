13539cf224714861af2563460456bdb7c0fb6cdf
Thai Nguyen <thainguyen2893@gmail.com>
2024-01-15 01:19:51 +0700

    Add new file

---
+++ sound.py
@@ -0,0 +1,9 @@
+import pygame
+
+# Sound effects
+pygame.mixer.init()
+die = pygame.mixer.Sound("sound/die.wav")
+hit = pygame.mixer.Sound("sound/hit.wav")
+point = pygame.mixer.Sound("sound/point.wav")
+swoosh = pygame.mixer.Sound("sound/swoosh.wav")
+wing = pygame.mixer.Sound("sound/wing.wav")
