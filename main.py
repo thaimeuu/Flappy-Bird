c20cc0c287b6693540ba8bf4511fc6643a6f2caa
Thai Nguyen <thainguyen2893@gmail.com>
2024-01-15 01:43:59 +0700

    Update file

--- main.py
+++ main.py
@@ -1,225 +1,188 @@
 import pygame
 from random import randint
 
+from bird import Bird
+from pipe import Pipe
+from sound import die, hit, point, swoosh, wing
+
+
+# Set up
 pygame.init()
 
-# Time
-CLOCK = pygame.time.Clock()
-FPS = 60
-
 # Surface
-HORIZONTAL, VERTICAL = (288, 580)
+HORIZONTAL, VERTICAL = 288, 580
 screen = pygame.display.set_mode((HORIZONTAL, VERTICAL))
 pygame.display.set_caption("flippy bard")
 
+# Time/Clock
+clock = pygame.time.Clock()
+FPS = 60
+
 # Sprites
-BACKGROUND = pygame.image.load("sprites/background-day.png")  # 288 x 512
+COUNTER = [  # 24 x 36
+    pygame.image.load("sprites/0.png"),
+    pygame.image.load("sprites/1.png"),
+    pygame.image.load("sprites/2.png"),
+    pygame.image.load("sprites/3.png"),
+    pygame.image.load("sprites/4.png"),
+    pygame.image.load("sprites/5.png"),
+    pygame.image.load("sprites/6.png"),
+    pygame.image.load("sprites/7.png"),
+    pygame.image.load("sprites/8.png"),
+    pygame.image.load("sprites/9.png"),
+]
+MESSAGE = pygame.image.load("sprites/message.png")
+GAMEOVER = pygame.image.load("sprites/gameover.png")
+RESTART = pygame.image.load("sprites/restart.png")  # 120 x 42
+RESTART = pygame.transform.scale(RESTART, (80, 28))  # 80 x 28
+RESTART_rect = RESTART.get_rect()
+BACKGROUND = pygame.image.load("sprites/background-day.png")
+BASE = pygame.image.load("sprites/base.png")
+BASE_POS = 0
+SCROLL_SPEED = 4
+bird = Bird(HORIZONTAL//2 - 80, VERTICAL//2 - 12, 34, 24)
 
-BASE = pygame.image.load("sprites/base.png")  # 336 x 112
-BASE_POSITION = 0
-BASE_SPEED = 4
+bot_pipe = Pipe(500, 350, -1)
+top_pipe = Pipe(500, 350, 1)
 
-PIPE_GAP = 100
+# Restart function
+def restart():
+    global click, is_inside, one, ten, bot_pipe, top_pipe
+    
+    # reset global variables
+    click = False  # user's click
+    is_inside = False
+    one = 0
+    ten = 0
+    
+    # reset sprites
+    bird.alive = True
+    bird.flying = False
+    bird.gravity = 0
+    bird.x, bird.y = HORIZONTAL//2 - 80, VERTICAL//2 - 12
+    
+    del bot_pipe
+    del top_pipe
+    
+    bot_pipe = Pipe(500, 350, -1)
+    top_pipe = Pipe(500, 350, 1)
 
-# Sound effects
-flap_sound = pygame.mixer.Sound("sound/wing.wav")
-hit_sound = pygame.mixer.Sound("sound/hit.wav")
-point_sound = pygame.mixer.Sound("sound/point.wav")
-
-
-class Bird:
-    img = [
-        pygame.image.load("sprites/yellowbird-downflap.png"),  # 34 x 24
-        pygame.image.load("sprites/yellowbird-midflap.png"),
-        pygame.image.load("sprites/yellowbird-upflap.png"),
-    ]
-
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.flyCount = 0
-        self.waiting = True
-        self.waiting_swingCount = 0
-        self.flap = False
-        self.hitbox = (self.x, self.y, self.width, self.height + 7)
-        self.gravity = 0
-        self.gameover = False
-        self.passPipe = False
-
-    def draw_motion(self, surface):
-        self.hitbox = (self.x, self.y, self.width, self.height + 7)
-        pygame.draw.rect(surface, "red", self.hitbox, 1)
-
-        # if not self.gameover:
-        if self.flyCount + 1 >= 30:
-            self.flyCount = 0
-
-        surface.blit(pygame.transform.rotate(self.img[self.flyCount // 10], self.gravity * -2.5), (self.x, self.y))
-        self.flyCount += 1
-
-        if not self.waiting:
-            if self.hitbox[1] + self.hitbox[3] <= 512:
-                self.y += int(self.gravity)
-                if self.gravity < 11:
-                    self.gravity += 0.25
-                else:
-                    self.gravity = 11
-                        
-        if self.gameover:
-            self.y += 10
-            
-        # elif self.gameover:
-        #     # Bird drop to ground
-        #     image = pygame.transform.rotate(self.img[1], -90)
-        #     surface.blit(image, (self.x, self.y))
-            
-        #     if self.hitbox[1] + self.hitbox[3] <= 512:
-        #             self.y += int(self.gravity)
-        #             if self.gravity < 11:
-        #                 self.gravity += 0.25
-        #             else:
-        #                 self.gravity = 11
-            
-
-    def hit(self):
-        # Reset bird
-        self.x = HORIZONTAL // 2 - 34 // 2 - 60
-        self.y = VERTICAL // 2 - 24 // 2
-        self.flyCount = 0
-        self.waiting = True
-        self.waiting_swingCount = 0
-        self.flap = False
-        self.gravity = 0
-
-        # Pause game for notification
-        i = 0
-        while i < 200:
-            pygame.time.delay(10)
-            i += 1
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    i = 201
-                    pygame.quit()
-
-        self.gameover = False
-
-
-class Pipe:
-    img = pygame.image.load("sprites/pipe-green.png")  # 52 x 320
-
-    def __init__(self, x, y, width, height, direction):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.direction = direction  # -1 down 1 up
-        self.hitbox = (self.x, self.y, self.width, self.height)
-
-    def draw_motion(self, surface):
-        # bot pipe
-        if self.direction == 1:
-            surface.blit(self.img, (self.x, self.y))
-            self.x -= BASE_SPEED
-            self.hitbox = (self.x + 4, self.y, self.width, self.height + 10)
-            pygame.draw.rect(surface, "red", self.hitbox, 1)
-            
-        # top pipe
-        else:
-            surface.blit(pygame.transform.flip(self.img, False, True), (self.x, self.y))
-            self.x -= BASE_SPEED
-            self.hitbox = (self.x + 4, self.y, self.width, self.height)
-            pygame.draw.rect(surface, "red", self.hitbox, 1)
-
-    def hit(self):
-        pass
-
-
-bird = Bird(HORIZONTAL // 2 - 34 // 2 - 60, VERTICAL // 2 - 24 // 2, 34, 24)
-
-pipes = [Pipe(425, -120, 52, 320, -1), Pipe(425, -120 + 320 + PIPE_GAP, 52, 320, 1)]
-
-
+# Main loop
 running = True
+click = False  # user's click
+is_inside = False
+# point-related
+one = 0
+ten = 0
 
 while running:
-    CLOCK.tick(FPS)
-
+    clock.tick(FPS)
+    
+    # LOGIC
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    for pipe in pipes:
-        if pipe.x + pipe.width < 0:
-            pipes.pop(pipes.index(pipe))
-
-    if not bird.gameover:
-        # Bird swinging up and down waiting for player to left-click
-        if bird.waiting:
-            if bird.waiting_swingCount < 25:
-                bird.y -= 0.3
-                bird.waiting_swingCount += 1
-            elif 25 <= bird.waiting_swingCount < 50:
-                bird.y += 0.3
-                bird.waiting_swingCount += 1
-            else:
-                bird.waiting_swingCount = 0
-
-        # Flap motion
-        if pygame.mouse.get_pressed()[0] and not bird.flap:
-            bird.flap = True
-            bird.waiting = False
+        elif event.type == pygame.MOUSEBUTTONDOWN and bird.flying == False and bird.alive == True:
+            bird.flying = True
+        # Check if restart is pressed
+        elif event.type == pygame.MOUSEBUTTONDOWN and RESTART_rect.collidepoint(event.pos) and not bird.flying and not bird.alive:
+            swoosh.play()
+            restart()
+     
+    # Generating pipes after pipes when bird's flying and alive
+    if bird.flying and bird.alive:
+        if bot_pipe.x + bot_pipe.width < 0:
+            del bot_pipe
+            del top_pipe
+            pipe_height = randint(300, 400)
+            bot_pipe = Pipe(HORIZONTAL, pipe_height, -1)
+            top_pipe = Pipe(HORIZONTAL, pipe_height, 1)
+     
+    # Allow bird to flap when it's alive (game not over)
+    if bird.alive:     
+        # bird.gravity helps avoid wing sound when clicking restart
+        if pygame.mouse.get_pressed()[0] and bird.gravity and click == False:
+            wing.play()
+            click = True
             bird.gravity = -5
-            flap_sound.play()
         elif not pygame.mouse.get_pressed()[0]:
-            bird.flap = False
+            click = False
+            
+        # Bird collides when hits pipes using hit base
+        if bird.hitbox[1] + bird.hitbox[3] >= 512 and bird.flying and bird.alive: 
+            hit.play()
+            die.play()
 
-        # Point
-        if pipes[0].x + pipes[0].width + 5 > bird.x + bird.width > pipes[0].x + pipes[0].width and bird.passPipe == False:
-            bird.passPipe = True
-            point_sound.play()
-        elif bird.passPipe == True:
-            temp = randint(-150, -50)
-            while len(pipes) < 3:
-                pipes.append(Pipe(288, temp, 52, 320, -1))  # top pipe
-                pipes.append(Pipe(288, temp + 320 + PIPE_GAP, 52, 320, 1))  # bot pipe
-            bird.passPipe = False
+            bird.gravity = 0
+            bird.alive = False
+            bird.flying = False
+            
+        # Bird collides when hits pipes using hit boxes
+        if (bot_pipe.hitbox[0] <= bird.hitbox[0] + bird.hitbox[2] <= bot_pipe.hitbox[0] + bot_pipe.hitbox[2] or
+            bot_pipe.hitbox[0] <= bird.hitbox[0] <= bot_pipe.hitbox[0] + bot_pipe.hitbox[2]):
+            
+            if (bird.hitbox[1] <= bot_pipe.hitbox[1] - bot_pipe.pipe_gap or
+                bird.hitbox[1] + bird.hitbox[3] >= bot_pipe.hitbox[1]):
+                
+                hit.play()
+                die.play()
 
-        # Losing/Collision
-        # or (bird.hitbox[1] <= pipes[0].y and pipes[0].x + pipes[0].width >= bird.hitbox[0] >= pipes[0].x)
-        if (bird.hitbox[1] + bird.hitbox[3] >= 512
-            
-            or ((bird.hitbox[1] <= pipes[0].hitbox[1] + pipes[0].hitbox[3]) and (pipes[0].hitbox[0] + pipes[0].hitbox[2]) >= (bird.hitbox[0] + bird.hitbox[2]) >= pipes[0].hitbox[0])
-            or ((bird.hitbox[1] <= pipes[0].hitbox[1] + pipes[0].hitbox[3]) and (pipes[0].hitbox[0] + pipes[0].hitbox[2]) >= bird.hitbox[0] >= pipes[0].hitbox[0])
-            
-            or ((bird.hitbox[1] + bird.hitbox[3] >= pipes[1].hitbox[1]) and (pipes[1].hitbox[0] + pipes[1].hitbox[2]) >= (bird.hitbox[0] + bird.hitbox[2]) >= pipes[1].hitbox[0])
-            or ((bird.hitbox[1] + bird.hitbox[3] >= pipes[1].hitbox[1]) and (pipes[1].hitbox[0] + pipes[1].hitbox[2]) >= bird.hitbox[0] >= pipes[1].hitbox[0])
-            ):
-            hit_sound.play()
-            
-            
-            
-            bird.gameover = True
-            bird.hit()
-            
-            del pipes
-            pipes = [Pipe(425, -120, 52, 320, -1), Pipe(425, -120 + 320 + PIPE_GAP, 52, 320, 1)]
-
-
+                bird.gravity = 0
+                bird.alive = False
+                bird.flying = False 
+    
+        # Point update
+        if bot_pipe.hitbox[0] + bot_pipe.hitbox[2] >= bird.hitbox[0] + bird.hitbox[2] >= bot_pipe.hitbox[0] and is_inside == False:
+            is_inside = True
         
-    # drawing()
+        if bird.hitbox[0] + bird.hitbox[2] > bot_pipe.hitbox[0] + bot_pipe.hitbox[2] and is_inside == True:
+            point.play()
+            one += 1
+            is_inside = False
+            if one > 9:
+                ten += 1
+                one = 0
+    
+    # DISPLAY 
     screen.blit(BACKGROUND, (0, 0))
-    if not bird.waiting:
-        for pipe in pipes:
-            pipe.draw_motion(screen)
-            
-    screen.blit(BASE, (BASE_POSITION, 512))
-    BASE_POSITION -= BASE_SPEED
-    if BASE_POSITION < -48:
-        BASE_POSITION = 0
-    bird.draw_motion(screen)
-
+    
+    # Message
+    if not bird.flying and bird.alive:
+        screen.blit(MESSAGE, (HORIZONTAL//2 - MESSAGE.get_size()[0]//2, 110))
+    
+    # Make sure update bird and pipes before displaying base so that bird isn't on top of base
+    bird.update(screen) 
+    if bird.flying and bird.alive: 
+        bot_pipe.update(screen)
+        top_pipe.update(screen)
+    else:  # if bird's dead, pipes stay still
+        bot_pipe.stay(screen)
+        top_pipe.stay(screen)
+          
+    # Bird collides when hits pipes when game over
+    if not bird.alive:
+        bird.collide(screen)
+              
+    screen.blit(BASE, (BASE_POS, 512))  
+    if bird.alive:
+        BASE_POS -= SCROLL_SPEED  # Scroll base when bird is alive
+        if BASE_POS < -48:
+            BASE_POS = 0
+    # Counter should be displayed at last
+    if not ten:    
+        screen.blit(COUNTER[one], (HORIZONTAL//2-COUNTER[one].get_size()[0]//2, 15))
+    elif ten:
+        screen.blit(COUNTER[ten], (HORIZONTAL//2-COUNTER[ten].get_size()[0], 15))
+        screen.blit(COUNTER[one], (HORIZONTAL//2, 15))
+    
+    # Game over
+    if not bird.alive and not bird.flying:
+        screen.blit(GAMEOVER, (HORIZONTAL//2 - GAMEOVER.get_size()[0]//2, VERTICAL//2 - GAMEOVER.get_size()[1]//2 - 60))
+        screen.blit(RESTART, (HORIZONTAL//2 - RESTART.get_size()[0]//2, VERTICAL//2 - RESTART.get_size()[1]//2))
+        RESTART_rect.center = (HORIZONTAL//2, VERTICAL//2)
+        
+        
     pygame.display.update()
-
+        
+    
 pygame.quit()
